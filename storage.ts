c7e80b960e8a89af7ee5837e4250efad66b541ac
as <arushshukla31@gmail.com>
2025-10-30 00:09:51 +0530

    Update storage.ts

--- storage.ts
+++ storage.ts
@@ -363,3 +363,10 @@ export const setSavingsEducationShown = (): void => {
     data.hasShownSavingsEducation = true;
     saveAppData(data);
 };
+export function getItem(key: string) {
+  return localStorage.getItem(key);
+}
+
+export function setItem(key: string, value: string) {
+  localStorage.setItem(key, value);
+}
